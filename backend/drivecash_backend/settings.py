e7c1e05a777efee3fda25547b642ef31c34bf5c9
murambaprogress <murambaprogress.com>
2025-11-01 20:49:22 +0200

    Update settings and .gitignore

--- backend/drivecash_backend/settings.py
+++ backend/drivecash_backend/settings.py
@@ -195,7 +195,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'murambaprogress@gmail.com'
-EMAIL_HOST_PASSWORD = 'oyxx bpdo lila mtrq'  # You'll need to generate an app password in your Google account
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Load from environment variable
 DEFAULT_FROM_EMAIL = 'DriveCash <murambaprogress@gmail.com>'
 
 # Custom user model
